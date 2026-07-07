e8dafcdda4b83a6caea5ba342471a63a13a1aec4
JuliusGarcia28 <julianjaviergarciaalvarez@gmail.com>
2026-07-06 18:52:30 -0700

    preparacion para produccion de la app web

--- Help_Desk_Frontend/src/app/environments/environment.ts
+++ Help_Desk_Frontend/src/app/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8000',
-  allowedImageDomains: ['localhost:8000', '127.0.0.1:8000'],
+  production: true,
+  //apiUrl: 'http://localhost:8000',
+  apiUrl: 'https://uas-helpdesk-backend.onrender.com',
+  allowedImageDomains: ['localhost:8000', '127.0.0.1:8000', 'uas-helpdesk-backend.onrender.com'],
 };
